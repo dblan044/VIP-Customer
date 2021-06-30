4cacc650886dc72e02e79fafad31bf20e9184fac
Diana R Blanco <54902184+dblan044@users.noreply.github.com>
2021-06-30 15:34:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,8 +13,8 @@ public class Main {
         System.out.println();
         
         VipCustomer acc3 = new VipCustomer("Diana", 10000, "db@email.com");
-        System.out.println(acc2.getName());
-        System.out.println(acc2.getCreditLimit());
-        System.out.println(acc2.getEmail() );
+        System.out.println(acc3.getName());
+        System.out.println(acc3.getCreditLimit());
+        System.out.println(acc3.getEmail() );
     }
 }
